62323c4ae6aeb3cb277caf734efc51324fdcb6ea
dhnascimento <danielhn@gmail.com>
2021-11-06 16:31:37 -0400

    completed module 3 (redux)

---
+++ src/todos/TodoList.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { connect } from "react-redux";
+import NewTodoForm from "./NewTodoForm";
+import TodoListItem from './TodoListItem';
+import { removeTodo, completeTodo } from "./actions";
+import './TodoList.css';
+
+const TodoList = ({ todos = [], onRemovePressed, onCompletePressed }) => (
+    <div className="list-wrapper">
+        <NewTodoForm />
+        {todos.map(todo => <TodoListItem
+            todo={todo}
+            onRemovePressed={onRemovePressed}
+            onCompletePressed={onCompletePressed}
+            />
+            )}
+    </div>
+);
+
+const mapStateToProps = state => ({
+    todos: state.todos
+});
+
+const mapDispatchToProps = dispatch => ({
+    onRemovePressed: text => dispatch(removeTodo(text)),
+    onCompletePressed: text => dispatch(completeTodo(text))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
